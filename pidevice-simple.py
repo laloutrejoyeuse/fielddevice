2fd692f7736a0f433fb0f70da4c108beca67ecc2
laloutrejoyeuse <laloutrejoyeuse@SPLUSTFHEMACBOOK.local>
2014-02-05 22:20:45 +0100

    Fully revised communication model (now client instead of server)

--- pidevice-simple.py
+++ pidevice-simple.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python
 from device import Device, IOManager
 from pifacedigitalio import PiFaceDigital
+from picamera import PiCamera
+from PIL import Image
+import io
+import time
+from threading import Event
+import logging
 
 class MyIOManager(IOManager):
 	def onInit(self):
 		self.pf=PiFaceDigital()
+		self.cam=PiCamera()
+		self.cam.resolution=(640,480)
 
 		# create inputs
-		self.li0=self.createInput('li0', 'bouton 0 carte piface (trigger lorsque down)')
+		self.li0=self.createInput('i0')
 		self.li0.setRunModeAsRaisingEdgeCounter()
-		self.li1=self.createInput('li1', 'bouton 1 carte piface (trigger lorsque impulsion 0->1->0)')
+
+		self.li1=self.createInput('i1')
 		self.li1.setRunModeAsImpulseCounter()
-		self.li2=self.createInput('li2', 'bouton 2 carte piface (trigger a chaque changement up<-->down)')
+
+		self.li2=self.createInput('i2')
 		self.li2.setRunModeAsEdgeCounter()
-		self.li3=self.createInput('li3', 'bouton 3 carte piface')
+
+		self.li3=self.createInput('i3')
+
+		self.cam0=self.createInputJPEG('im0')
+		self.jobCam=self.job(self.piCamManager)
+		self.eventShoot=Event()
 
 		# create outputs
-		self.lo0=self.createOutput('lo0', 'relai 0 piface')
+		self.lo0=self.createOutput('o0')
+		self.lo1=self.createOutput('o1')
 
-		self.lo0.value=1
+		#self.job2=self.job(self.test)
 
+ 
 	def onRun(self):
 		# process inputs
 		self.li0.value=self.pf.input_pins[0].value
@@ -29,7 +46,7 @@ class MyIOManager(IOManager):
 
 		# process outputs
 		for io in self.inputs():
-			if io.checkAndClearTrigger():
+			if io.index<4 and io.checkAndClearTrigger():
 				self.lo0.toggle()
 
 		if self.li3.value:
@@ -39,23 +56,50 @@ class MyIOManager(IOManager):
 		self.sleep(0.01)
 
 	def onUpdateOutput(self, io):
-		print "onUpdateOutput(%s)->%f" % (io.name, io.value)
+		self.logger.debug("onUpdateOutput(%s)->%f" % (io.name, io.value))
 		if io==self.lo0:
 			self.pf.output_pins[0].value=io.value
+		if io==self.lo1:
+			self.pf.output_pins[1].value=io.value
+			self.eventShoot.set()
 
 	def onStop(self):
 		# do a clean exit
 		self.resetAllOutputs()
 
 	def onRelease(self):
-		pass
+		self.cam.close()
+
+	def piCamManager(self):
+		while not self.isStopRequest():
+			self.eventShoot.clear()
+			self.logger.debug('picam:capture...')
+			stream=io.BytesIO()
+			self.cam.led=1
+			self.cam.capture(stream, format='jpeg')
+			stream.seek(0)
+			value=stream.getvalue()
+			stream.close()
+			self.cam0.value=value
+			self.cam.led=0
+			#self.eventShoot.wait(5)
+
+	def test(self):
+		i=5
+		while i>0:
+			print "TEST %d" % i
+			time.sleep(1)
+			i-=1
 	
 
-dev=Device(MyIOManager, 8888)
-dev.allowRemoteShutdown(True)
+dev=Device('https://192.168.2.1/rws/api/dcf', 
+	's_STDEMO_fd2', 
+	'BFEBB90A6F1876531F14225D6AAF3BE0', 
+	MyIOManager,
+	'192.168.0.84',
+	logging.DEBUG)
 
 dev.addFileToScriptUpdateMonitor(__file__)
 dev.enableShutdownOnScriptUpdate()
 
 dev.start()
-
